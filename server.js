d026ad5a01b8b8c979a9c84acd1f929eb382d294
Will <willst.willst@gmail.com>
2017-04-17 16:41:30 +0100

    Complete walking skeleton section of tutorial

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express'), 
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('multivision db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' +  req.params.partialPath);
+});
+
+// For any requests from the client render and respond with the index view.
+app.get('*', function(req, res) {
+    res.render('index', { mongoMessage: mongoMessage});
+});
+
+var port = 3030;
+app.listen(3030);
+
+console.log('Listening to port' + port + '...');
